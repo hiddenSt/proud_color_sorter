9c03742ae6a84a3a31c9d784018cc6a9f7074a12
Viktor Repin <hiddenstmail@gmail.com>
2023-01-19 01:16:53 +0700

    Add more methods to Order iterator types

--- src/order.hpp
+++ src/order.hpp
@@ -24,8 +24,8 @@ class Order {
    public:
     using iterator_category = std::random_access_iterator_tag;  // NOLINT
     using value_type = T;                                       // NOLINT
-    using pointer = T*;                                         // NOLINT
-    using reference = T&;                                       // NOLINT
+    using pointer = value_type*;                                // NOLINT
+    using reference = value_type&;                              // NOLINT
     using difference_type = std::ptrdiff_t;                     // NOLINT
 
     explicit Iterator(T* rank_to_element);
@@ -36,29 +36,84 @@ class Order {
     Iterator& operator++();
     Iterator operator++(int);
 
+    Iterator& operator--();
+    Iterator operator--(int);
+
+    Iterator& operator+=(difference_type n);
+    Iterator& operator-=(difference_type n);
+
     friend bool operator==(const Iterator& lhs, const Iterator& rhs);
     friend bool operator!=(const Iterator& lhs, const Iterator& rhs);
+    friend bool operator==(const Iterator& lhs, const Iterator& rhs);
+    friend bool operator!=(const Iterator& lhs, const Iterator& rhs);
+    friend bool operator<(const Iterator& lhs, const Iterator& rhs);
+    friend bool operator<=(const Iterator& lhs, const Iterator& rhs);
+    friend bool operator>(const Iterator& lhs, const Iterator& rhs);
+    friend bool operator>=(const Iterator& lhs, const Iterator& rhs);
+
+    friend Iterator operator+(const Iterator& lhs, difference_type rhs);
+    friend Iterator operator+(difference_type lhs, const Iterator& rhs);
+    friend Iterator operator-(const Iterator& lhs, difference_type rhs);
+    friend difference_type operator-(const Iterator& lhs, const Iterator& rhs);
 
    private:
     T* rank_to_element_ptr_ = nullptr;
   };
 
-  Iterator begin();
-  Iterator end();
+  class ConstIterator {
+   public:
+    using iterator_category = std::random_access_iterator_tag;  // NOLINT
+    using value_type = const T;                                 // NOLINT
+    using pointer = value_type*;                                // NOLINT
+    using reference = value_type&;                              // NOLINT
+    using difference_type = std::ptrdiff_t;                     // NOLINT
 
-  Iterator rbegin();
-  Iterator rend();
+    explicit ConstIterator(const T* rank_to_element);
 
-  // ConstIterator cbegin() const
-  // ConstIterator cend() const
-  // ConstIterator begin() const
-  // ConstIterator end() const
-  // Iterator rbegin()
-  // Iterator rend()
-  // ConstIterator crbegin() const
-  // ConstIterator crend() const
-  // ConstIterator rbegin() const
-  // ConstIterator rend() const
+    reference operator*() const;
+    pointer operator->() const;
+
+    ConstIterator& operator++();
+    ConstIterator operator++(int);
+
+    ConstIterator& operator--();
+    ConstIterator operator--(int);
+
+    ConstIterator& operator+=(difference_type n);
+    ConstIterator& operator-=(difference_type n);
+
+    friend bool operator==(const ConstIterator& lhs, const ConstIterator& rhs);
+    friend bool operator!=(const ConstIterator& lhs, const ConstIterator& rhs);
+    friend bool operator==(const ConstIterator& lhs, const ConstIterator& rhs);
+    friend bool operator!=(const ConstIterator& lhs, const ConstIterator& rhs);
+    friend bool operator<(const ConstIterator& lhs, const ConstIterator& rhs);
+    friend bool operator<=(const ConstIterator& lhs, const ConstIterator& rhs);
+    friend bool operator>(const ConstIterator& lhs, const ConstIterator& rhs);
+    friend bool operator>=(const ConstIterator& lhs, const ConstIterator& rhs);
+
+    friend ConstIterator operator+(const ConstIterator& lhs, difference_type rhs);
+    friend ConstIterator operator+(difference_type lhs, const ConstIterator& rhs);
+    friend ConstIterator operator-(const ConstIterator& lhs, difference_type rhs);
+    friend difference_type operator-(const ConstIterator& lhs, const ConstIterator& rhs);
+
+   private:
+    const T* rank_to_element_ptr_ = nullptr;
+  };
+
+  Iterator begin() { return Iterator{rank_to_element_.data()}; }  // NOLINT
+  Iterator end() { return Iterator{}; };                          // NOLINT
+
+  // Iterator rbegin();  // NOLINT
+  // Iterator rend();    // NOLINT
+
+  ConstIterator cbegin() const { return ConstIterator{rank_to_element_.data()}; };  // NOLINT
+  ConstIterator cend() const { return ConstIterator{}; };                           // NOLINT
+  ConstIterator begin() const { return ConstIterator{rank_to_element_.data()}; };   // NOLINT
+  ConstIterator end() const { return ConstIterator{}; };                            // NOLINT
+  // ConstIterator crbegin() const;  // NOLINT
+  // ConstIterator crend() const;    // NOLINT
+  // ConstIterator rbegin() const;   // NOLINT
+  // ConstIterator rend() const;     // NOLINT
 
  private:
   std::array<T, MaxRank> rank_to_element_;
