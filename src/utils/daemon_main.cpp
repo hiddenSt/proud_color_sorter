4146ecb793c8dac5ec8fe63ef54292f2f5358c0a
Viktor Repin <hiddenstmail@gmail.com>
2023-01-20 01:29:17 +0700

    Add scratch main

--- src/utils/daemon_main.cpp
+++ src/utils/daemon_main.cpp
@@ -1,22 +1,93 @@
+#include <utils/color_formatter.hpp>
 #include <utils/daemon_main.hpp>
 
-#include <csignal>
-#include <cstdlib>
-#include <mutex>
+#include <random>
 #include <thread>
 
-#include <CLI/App.hpp>
+#include <fmt/core.h>
+#include <fmt/format.h>
+#include <CLI/CLI.hpp>
+
+#include <color.hpp>
+#include <counting_sort.hpp>
+#include <order.hpp>
+#include <spsc_queue.hpp>
 
 namespace proud_color_sorter::utils {
 
+using Channel = SPSCUnboundedBlockingQueue<std::vector<Color>>;
+
+template <typename T>
+struct RandomGenerator {
+  RandomGenerator(T a, T b) : uniform_distribution(a, b), mersenne_twister(rand_device()) {}
+
+  std::random_device rand_device;
+  std::mt19937 mersenne_twister;
+  std::uniform_int_distribution<std::size_t> uniform_distribution;
+
+  T Generate() { return uniform_distribution(mersenne_twister); }
+};
+
+namespace detail {
+
+std::vector<Color> GenerateColors(RandomGenerator<std::size_t>& size_generator,
+                                  RandomGenerator<std::uint8_t>& color_generator) {
+  auto color_size = size_generator.Generate();
+  std::vector<Color> colors;
+  colors.reserve(color_size);
+
+  std::uniform_int_distribution<std::uint8_t> uniform_distribution_element{0, kColorCount};
+
+  for (std::size_t i = 0; i < color_size; ++i) {
+    auto generated = color_generator.Generate();
+    colors.emplace_back(static_cast<Color>(generated));
+  }
+
+  return colors;
+}
+
+void Consume(Channel& channel, const ColorOrder& order) {
+  while (true) {
+    auto colors = channel.Take();
+
+    if (!colors.has_value()) {
+      return;
+    }
+
+    fmt::print("Generated colors (size={}): {} \n", colors->size(), fmt::join(colors.value(), " "));
+    auto sorted_colors = CountingSort(colors.value(), order);
+    fmt::print("Sorted colors (size={}): {} \n", sorted_colors.size(), fmt::join(sorted_colors, " "));
+  }
+}
+
+}  // namespace detail
+
 int DaemonMain(int argc, char* argv[]) {
   CLI::App app{"App for sorting random generated colors."};
+  std::size_t generated_seq_max_size = 0;
+  app.add_option("--max_size", generated_seq_max_size, "Max length of generated color sequence.")->default_val(100);
+  // app.add_option("--colors_order", );
+  CLI11_PARSE(app, argc, argv);
 
-  std::size_t num_threads = 1;
-  app.add_option("--num-threads", num_threads, "Number of threads to ");
-  //
-  // while (thread_pool)
-  return EXIT_FAILURE;
+  Channel channel;
+  ColorOrder color_order;
+  color_order.Set(Color::kRed, 0);
+  color_order.Set(Color::kGreen, 1);
+  color_order.Set(Color::kBlue, 2);
+
+  auto producer = std::thread([&channel, generated_seq_max_size]() mutable {
+    std::vector<Color> colors;
+    RandomGenerator<std::size_t> size_generator{1, generated_seq_max_size};
+    RandomGenerator<std::uint8_t> color_generator{0, kColorCount - 1};
+
+    do {
+      colors = detail::GenerateColors(size_generator, color_generator);
+    } while (channel.Put(std::move(colors)));
+  });
+
+  detail::Consume(channel, color_order);
+
+  return EXIT_SUCCESS;
 }
 
 }  // namespace proud_color_sorter::utils
--- src/utils/daemon_main.hpp
+++ src/utils/daemon_main.hpp
@@ -2,6 +2,7 @@
 
 namespace proud_color_sorter::utils {
 
+/// Parses command line arguments and starts application.
 int DaemonMain(int argc, char* argv[]);
 
 }  // namespace proud_color_sorter::utils
