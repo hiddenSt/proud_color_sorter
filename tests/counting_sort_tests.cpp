84677aae4fad83e44afe648376e276fd13706cab
Viktor Repin <hiddenstmail@gmail.com>
2023-01-19 17:03:23 +0700

    Add more tests

--- tests/counting_sort_tests.cpp
+++ tests/counting_sort_tests.cpp
@@ -1,23 +1,38 @@
 #include <array>
+#include <algorithm>
 #include <vector>
 
 #include <gtest/gtest.h>
 
 #include <counting_sort.hpp>
+#include <order.hpp>
 
 namespace proud_color_sorter::tests {
 
 TEST(CountingSortTest, empty_array) {
   std::vector<Color> colors;
-  std::array<Color, kColorCount> color_order = {Color::kBlue, Color::kRed, Color::kGreen};
-
-  auto sorted_colors = CountingSort(colors, color_order);
+  ColorOrder order;
+  order.Set(Color::kRed, 0);
+  order.Set(Color::kGreen, 1);
+  order.Set(Color::kBlue, 2);
+  auto sorted_colors = CountingSort(colors, order);
 
   ASSERT_TRUE(sorted_colors.empty());
 }
 
-TEST(CountingSortTest, empty_order) {}
+TEST(CountingSortTest, order) {
+  std::vector<Color> colors{Color::kRed, Color::kGreen, Color::kBlue, Color::kGreen, Color::kBlue};
+  ColorOrder order;
+  order.Set(Color::kRed, 0);
+  order.Set(Color::kGreen, 1);
+  order.Set(Color::kBlue, 2);
 
-TEST(CountingSortTest, order_test) {}
+  auto sorted_colors = CountingSort(colors, order);
+  std::sort(colors.begin(), colors.end(), [&order](const Color lhs, const Color rhs) mutable {
+    return order.IsLess(lhs, rhs);
+  });
+
+  EXPECT_EQ(sorted_colors, colors);
+}
 
 }  // namespace proud_color_sorter::tests
