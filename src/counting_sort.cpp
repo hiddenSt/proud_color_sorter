6e8fd3d0271613dfb23d5fb0a30ae2c0cf318f81
Viktor Repin <hiddenstmail@gmail.com>
2023-01-19 21:57:40 +0700

    Add initialization for color_count array

--- src/counting_sort.cpp
+++ src/counting_sort.cpp
@@ -1,11 +1,17 @@
 #include <counting_sort.hpp>
 
+#include <cstdlib>
+
 namespace proud_color_sorter {
 
 namespace detail {
 
 std::array<std::size_t, kColorCount> CountColors(const std::vector<Color>& colors) {
   std::array<std::size_t, kColorCount> color_count;
+
+  for (std::size_t i = 0; i < color_count.size(); ++i) {
+    color_count[i] = 0;
+  }
 
   for (const auto& color : colors) {
     ++color_count[static_cast<std::size_t>(color)];
@@ -28,7 +34,7 @@ std::vector<Color> CountingSort(const std::vector<Color>& colors, const ColorOrd
 
   std::vector<Color> sorted_colors;
   sorted_colors.reserve(colors.size());
-
+  
   for (const Color color : color_order) {
     detail::AddColorTo(sorted_colors, color_count[static_cast<std::size_t>(color)], color);
   }
--- src/counting_sort.hpp
+++ src/counting_sort.hpp
@@ -10,17 +10,20 @@ namespace proud_color_sorter {
 
 using ColorOrder = Order<Color, kColorCount>;
 
-// Note: This task can be solved using `std::sort` like this:
-// ```cpp
-// std::vector<Color> colors;
-// ColorOrder color_order;
-// std::sort(colors.begin(),
-//           colors.end(),
-//           [&color_order](const Color lhs, const Color rhs) {
-//             return color_order.IsLess(lhs, rhs);
-//
-// });
-// ```
+/// Sorts \a colors using \a color_order using counting sort algorithm.
+///
+/// Creates a new vector of sorted colors.
+/// \note: This task can be solved using `std::sort` like this:
+/// ```cpp
+/// std::vector<Color> colors;
+/// ColorOrder color_order;
+/// std::sort(colors.begin(),
+///           colors.end(),
+///           [&color_order](const Color lhs, const Color rhs) {
+///             return color_order.IsLess(lhs, rhs);
+///
+/// });
+/// ```
 std::vector<Color> CountingSort(const std::vector<Color>& colors, const ColorOrder& color_order);
 
 }  // namespace proud_color_sorter
