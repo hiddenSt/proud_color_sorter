a89e97e7b4b015ca7df1a710c7eef065477cebeb
Viktor Repin <hiddenstmail@gmail.com>
2023-01-21 18:56:12 +0700

    Refactor app start up code

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,27 @@
-#include <utils/daemon_main.hpp>
+#include <stdexcept>
 
-int main(int argc, char* argv[]) { return proud_color_sorter::utils::DaemonMain(argc, argv); }
+#include <fmt/core.h>
+#include <fmt/format.h>
+#include <CLI/CLI.hpp>
+
+#include <utils/app.hpp>
+
+int main(int argc, char* argv[]) {
+  constexpr std::size_t kMaxGeneratedSequenceLength = 100;
+
+  proud_color_sorter::utils::Config config;
+  CLI::App app{"App for sorting random generated colors."};
+  app.add_option("--max_size", config.generated_seq_max_size, "Max length of generated color sequence.")
+      ->default_val(kMaxGeneratedSequenceLength);
+  // app.add_option("--colors_order", );
+  CLI11_PARSE(app, argc, argv);
+
+  try {
+    proud_color_sorter::utils::RunApp(config);
+  } catch (const std::exception& error) {
+    fmt::print(stderr, "Exception caught: {}", error.what());
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
--- src/utils/daemon_main.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace proud_color_sorter::utils {
-
-/// Parses command line arguments and starts application.
-int DaemonMain(int argc, char* argv[]);
-
-}  // namespace proud_color_sorter::utils
